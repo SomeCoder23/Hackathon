ad01f5dea8cee33e995a9df7831e4c65a707575f
SomeCoder23 <22111905@students.hebron.edu>
2023-09-02 13:54:27 +0300

    Added more features

--- index.ts
+++ index.ts
@@ -1,9 +1,205 @@
-var AWS = require("aws-sdk");
+import express from 'express';
+import multer from 'multer';
+import fs from 'fs';
+const AWS = require("aws-sdk");
+AWS.config.update({ region: 'us-west-2' });
+const rekognition = new AWS.Rekognition();
+console.log('Region: ' + AWS.config.region);
 
-AWS.config.getCredentials(function(err: { stack: any; }) {
-  if (err) console.log(err.stack);
-  // credentials not loaded
-  else {
-    console.log("Access key:", AWS.config.credentials.accessKeyId);
+var app = express();
+
+const PORT = 5000;
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'uploads/');
+  },
+  filename: (req, file, callback) => {
+    callback(null, Date.now() + '-' + file.originalname)
   }
 });
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    res.status(500).send("Failed Upload File!");
+    return;
+  }
+  const fileURL = req.file.destination + req.file.filename;
+  res.send({
+    message: 'File Uploaded Successfully!',
+    file: fileURL
+  });
+});
+
+/*app.get('/label', (req, res) => {
+
+  const params = {  Image: {
+    Bytes: '',  },
+};
+rekognition.detectLabels(params, (err: any, data: { Labels: any; }) => {  
+  if (err) {
+    console.error('Error detecting labels:', err);  
+    res.send("Something went wrong");
+  } else {
+    console.log('Labels detected:', data.Labels);  
+    res.send(data.Labels);
+  }
+});
+
+});*/
+
+app.post('/labels', upload.single('file'), async (req, res) => {
+  try {
+    // Check if a file was uploaded in the request
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded.' });
+    }
+
+    // Read the uploaded image file
+    const imageBytes = fs.readFileSync(req.file.path);
+
+    // Detect labels using Rekognition
+    const params = {
+      Image: {
+        Bytes: imageBytes
+      },
+      MaxLabels: 10, // Adjust this value as needed
+      MinConfidence: 70 // Adjust this value as needed
+    };
+
+    const labelsResponse = await rekognition.detectLabels(params).promise();
+
+    // Send the detected labels as a JSON response
+    res.json({ labels: labelsResponse.Labels });
+  } catch (error) {
+    console.error('Error detecting labels:', error);
+    res.status(500).json({ error: 'Error detecting labels.' });
+  }
+});
+
+app.post('/celebs', upload.single('file'), async (req, res) => {
+  try {
+    // Check if a file was uploaded in the request
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded.' });
+    }
+
+    const imageBytes = fs.readFileSync(req.file.path);
+    const params = {
+      Image: {
+        Bytes: imageBytes
+      },
+      
+    };
+    rekognition.recognizeCelebrities(params, (err: any, data: { CelebrityFaces: { Name: any; MatchConfidence: any; }[]; }) => { 
+      if (err) { 
+        console.error('Error spotting celebrities:', err); 
+      } else { 
+        if (data.CelebrityFaces.length === 0) { 
+          console.log('No celebrities found in the image.');
+          res.send('No celebrities found in the image.');        
+        } else { 
+          console.log('Celebrities found:'); 
+          res.send(data.CelebrityFaces);
+         
+        } 
+      } 
+      }); 
+
+  } catch(err){
+      console.log("ERROR: " + err);
+      res.send(err);
+  }
+
+
+
+  
+});
+
+app.post('/text', upload.single('file'), async (req, res) => {
+  try {
+    // Check if a file was uploaded in the request
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded.' });
+    }
+
+    const imageBytes = fs.readFileSync(req.file.path);
+    const params = {
+      Image: {
+        Bytes: imageBytes
+      },
+      
+    };
+   rekognition.detectText(params, (err: any, data: { TextDetections: any[]; }) => { 
+    if (err) { 
+      console.error('Error extracting text:', err); 
+    } else { 
+      if (data.TextDetections.length === 0) { 
+        console.log('No text found in the image.'); 
+        res.send("No text detected");
+      } else { 
+        console.log('Text detected:'); 
+        res.send(data.TextDetections);
+      } 
+    } 
+  });
+
+  } catch(err){
+      console.log("ERROR: " + err);
+      res.send(err);
+  }
+
+
+
+  
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
+
+export default app;
+
+//const imageFilePath = 'uploads/1693647378038-alienYellow_stand.png';
+  //const imageBuffer = fs.readFileSync(imageFilePath);
+  //console.log("IMAGE: " + imageBuffer);
+  /*const params = {  Image: {
+    Bytes: "",  },
+};
+
+rekognition.detectLabels(params, (err: any, data: { Labels: any; }) => {  
+  if (err) {
+    console.error('Error detecting labels:', err); 
+ } else {
+    console.log('Labels detected:', data.Labels); 
+   }
+  });*/
+
+
+
+
+
+/*var AWS = require("aws-sdk");
+
+AWS.config.update({ region: 'us-west-2' });
+console.log('Region: ' + AWS.config.region);
+
+const imageBase64 = ''
+const imageBytes = Buffer.from(imageBase64, 'base64');
+const params = {  Image: {
+    Bytes: imageBytes,  },
+};
+rekognition.detectLabels(params, (err: any, data: { Labels: any; }) => {  if (err) {
+    console.error('Error detecting labels:', err);  } else {
+    console.log('Labels detected:', data.Labels);  }
+});*/
+
